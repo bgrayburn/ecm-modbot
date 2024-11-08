0a83f809e6dfb8a2f2cb4b74539ee47201469c46
Brian Rayburn <bgrayburn@gmail.com>
2024-11-07 19:06:53 -0500

    create empty class

---
+++ src/votingSystem.ts
@@ -0,0 +1,8 @@
+/*
+  This class determines the voting system for policies for the moderation bot.
+  It determines whether or not a policy is approved or denied based on the
+  number of votes it has received.
+*/
+export default class VotingSystem {
+
+}
